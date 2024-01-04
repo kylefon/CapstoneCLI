d8a588c15741fdd28413688d180f21a1981a7525
ky <robnekyle@gmail.com>
2024-01-04 14:44:34 +0800

    Add generate link function

--- clouduploader.sh
+++ clouduploader.sh
@@ -2,31 +2,6 @@
 
 source config.sh
 
-#check errors during upload
-check_error() {
-    if [ $? -ne 0 ]; then
-    echo "Error: The command failed."
-    else
-        echo "Success: The command succeeded."
-    fi
-}
-
-#lists blobs in container
-blob_list(){
-    az storage blob list --account-name $accName --container-name $conName --output tsv --auth-mode login --query "[].name"
-}
-
-#upload files to container 
-upload() {
-    if [ -f $FILEPATH ]; then
-        echo "Uploading $FILEPATH..."
-        pv $FILEPATH | az storage blob upload --account-name $accName --container-name $conName --name "$(basename "$FILEPATH")" --type block --file "$FILEPATH" --auth-mode login 2>/dev/null
-        check_error
-    else
-        echo "$FILEPATH does not exist"
-    fi
-}
-
 #checks if file is already in container
 check_storage() {
     for FILEPATH in "$@"; do 
@@ -42,8 +17,58 @@ check_storage() {
             echo "$FILEPATH already exists"
         else
             upload "$FILEPATH"
+
+            read -p "Do you want to generate a shareable link for $FILEPATH? (Y/N):" userInput
+
+            case $userInput in
+                [Yy])
+                    generate_link "$FILEPATH"
+                    ;;
+                [Nn])
+                    echo "Skipping link generation"
+                    ;;
+                *)
+                    echo "Invalid input. Skipping link generation"
+                    ;;
+            esac
         fi
     done 
+}
+
+#check errors during upload
+check_error() {
+    if [ $? -ne 0 ]; then
+    echo "Error: The command failed."
+    else
+        echo "Success: The command succeeded."
+    fi
+}
+
+#upload files to container 
+upload() {
+    if [ -f $FILEPATH ]; then
+        echo "Uploading $FILEPATH..."
+        pv $FILEPATH | az storage blob upload --account-name $accName --container-name $conName --name "$(basename "$FILEPATH")" --type block --file "$FILEPATH" --auth-mode login 2>/dev/null
+        check_error
+    else
+        echo "$FILEPATH does not exist"
+    fi
+}
+
+generate_link() {
+    local FILEPATH="$1"
+    #Generate SAS
+    local SAS_TOKEN=$(az storage blob generate-sas --account-name "$accName" --container-name "$conName" --name "$(basename $FILEPATH)" --auth-mode login  --as-user --permissions r --expiry "$(date -u --date='1 day' '+%Y-%m-%dT%H:%MZ')" --output tsv)
+    #Display link
+    local LINK="https://$accName.blob.core.windows.net/$conName/$(basename $FILEPATH)?$SAS_TOKEN"
+    echo "Link for $FILEPATH"
+    echo "$LINK"
+}
+
+
+#lists blobs in container
+blob_list(){
+    az storage blob list --account-name $accName --container-name $conName --output tsv --auth-mode login --query "[].name"
 }
 
 check_storage "$@"
