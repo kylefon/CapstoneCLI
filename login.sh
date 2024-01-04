845880228d52d7a498a9dcbdb29ebb143d595ba4
ky <robnekyle@gmail.com>
2024-01-04 13:00:31 +0800

    Add login script

--- login.sh
+++ login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 # Install az cli
-#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 # Login
 az login --use-device-code
 echo "You're logged in."
